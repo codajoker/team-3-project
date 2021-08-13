a6ddc8e1a18cab0a157ee6af0ddab73ff22be89a
codajoker <84983434+codajoker@users.noreply.github.com>
2021-08-13 07:30:20 +0000

    Deploying to gh-pages from @ codajoker/team-3-project@393d6f7f39187c6b51d71c299311724e40e4a705 🚀

--- src.cd10699e.js
+++ src.cd10699e.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./utils/_variables.scss":"cOvS","./utils/_fonts.scss":"l31s"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-3-project/src.1f6ea365.js.map
+//# sourceMappingURL=/team-3-project/src.cd10699e.js.map
